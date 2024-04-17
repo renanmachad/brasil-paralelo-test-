90fc98cfeff65d31d6403bb4e6df1b1f9b9bf28a
renanmachad <renanmachadopinho@gmail.com>
2024-04-17 01:27:24 -0300

    :feature update SQL script to make cast to UUID type and add COESCALE function to make query more generic

--- src/playback_project/domain/database/sql/positions-operations.sql
+++ src/playback_project/domain/database/sql/positions-operations.sql
@@ -8,23 +8,21 @@ SELECT user_id,
        position,
        finished
 FROM playback.positions
-WHERE user_id = :user_id_param
-  AND media_id = :media_id_param
-  AND title_id = :title_id_param
-  AND finished = :finished_param
+WHERE (user_id = COALESCE(:user_id::UUID, user_id))
+  AND (media_id = COALESCE(:media_id::UUID, media_id))
+  AND (title_id = COALESCE(:title_id::UUID, title_id))
+  AND (finished = COALESCE(:finished::boolean, finished))
 LIMIT :per_page OFFSET :page * :per_page;
-
-
 -- A Calculate total pages
--- :name x-total-pages
+-- :name query-total-pages
 -- :doc Calculate total of pages based on parameters
 -- :result :one
 SELECT (COUNT(*) / :per_page) AS total_pages
 FROM playback.positions
-WHERE user_id = :user_id_param
-  AND media_id = :media_id_param
-  AND title_id = :title_id_param
-  AND finished = :finished_param;
+WHERE user_id = :user_id::uuid
+  AND media_id = :media_id::uuid
+  AND title_id = :title_id::uuid
+  AND finished = :finished;
 
 
 -- :name insert-position
