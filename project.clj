dc370db9d2e833c8d05c5c56dd8993b48e64728a
renanmachad <renanmachadopinho@gmail.com>
2024-04-17 01:24:54 -0300

    :feature Config profiles and uberJar

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.layerware/hugsql "0.5.1"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.1"]
@@ -10,9 +10,19 @@
                  [compojure "1.6.1"]
                  [http-kit/http-kit "2.3.0"]
                  [ring/ring-defaults "0.3.2"]
+                 [ring/ring-mock "0.4.0"]
                  [ring/ring-json "0.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [log4j/log4j "1.2.16"]
                  [funcool/clojure.jdbc "0.9.0"]
                  [org.clojure/data.json "0.2.6"]]
-  :repl-options {:init-ns playback-project.core})
+  :main ^:skip-aot playback-project.core
+  :uberjar-name "playback-project-standalone.jar"
+  :target-path "target/%s"
+  :repl-options {:init-ns playback-project.core}
+  :profiles {
+             :uberjar {:aot      :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                       }
+             :test    {:env {:test true}}}
+  )
